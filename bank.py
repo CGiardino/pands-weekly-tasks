5d925edb6977ed60bbca92b709d0e8436c7ec6ce
Carmine Giardino <carmine.giardino@ocamsoft.com>
2025-02-04 22:23:28 +0000

    added bank.py for weekly tasks 02

---
+++ bank.py
@@ -0,0 +1,8 @@
+try:
+    amount1 = int(input("Enter amount1(in cent):"))
+    amount2 = int(input("Enter amount2(in cent):"))
+    total_cents = amount1 + amount2
+    total_euros = total_cents / 100
+    print("The sum of these is €", total_euros)
+except ValueError:
+    print("Error: Please enter a valid integer amount.")
